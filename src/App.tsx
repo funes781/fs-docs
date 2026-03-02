2a2b50707fda71e2a9766be12c3d6e1eac57ca4d
funes781 <121256208+funes781@users.noreply.github.com>
2026-03-02 17:10:10 +0100

    HashRouting

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "./components/ui/toaster";
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
 
@@ -19,12 +20,18 @@ const App = () => (
         <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter basename="/fs-docs">
+            {/* <BrowserRouter basename="/fs-docs">
                 <Routes>
                     <Route path="/" element={<Index />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
-            </BrowserRouter>
+            </BrowserRouter> */}
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </HashRouter>
         </TooltipProvider>
     </QueryClientProvider>
 );
