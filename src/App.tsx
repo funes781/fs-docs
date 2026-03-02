26618a5f027b8e495dab3ca308055acaf267c613
funes781 <121256208+funes781@users.noreply.github.com>
2026-03-02 17:01:32 +0100

    deployment fix

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
         <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter>
+            <BrowserRouter basename="/fs-docs">
                 <Routes>
                     <Route path="/" element={<Index />} />
                     <Route path="*" element={<NotFound />} />
